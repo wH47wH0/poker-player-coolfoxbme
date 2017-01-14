973f59eb6ebff4c6b3b2116fa585a50bf269c515
Berkecz Ádam <berkecz.adam@autsoft.hu>
2017-01-14 11:21:45 +0100

    betRequest refactor

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,10 +1,6 @@
 package org.leanpoker.player;
 
-import java.util.List;
-
-import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
 
 public class Player {
 
@@ -13,17 +9,10 @@ public class Player {
     public static final double BIG_BET_TRESHOLD = 0.2;
 
     public static int betRequest(GameState gameState) {
-        double ourBet = 0;
+        double ourBet = getOurPlayer(gameState).getBet();
 
-        List<Opponent> players = gameState.getPlayers();
-        for (Opponent player : players) {
-            if (player.getStatus().equals(IN_ACTION)) {
-                ourBet = player.getBet();
-            }
-        }
         double betToCall = gameState.getCurrentBuyIn();
         return (int) (betToCall - ourBet + gameState.getMinimumRaise());
-
     }
 
     public static void showdown(JsonElement game) {
