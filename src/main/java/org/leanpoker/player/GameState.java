de9afb13e39c5b4774dc063656b1309543dd4763
Berkecz Ádam <berkecz.adam@autsoft.hu>
2017-01-14 10:30:22 +0100

    fix

--- src/main/java/org/leanpoker/player/GameState.java
+++ src/main/java/org/leanpoker/player/GameState.java
@@ -1,8 +1,10 @@
 package org.leanpoker.player;
 
+import java.util.List;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.sun.tools.javac.util.List;
+
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class GameState {
@@ -104,7 +106,7 @@ public class GameState {
         return inAction;
     }
 
-    public List<Opponent> getOtherPlayers() {
+    public List<Opponent> getPlayers() {
         return otherPlayers;
     }
 
