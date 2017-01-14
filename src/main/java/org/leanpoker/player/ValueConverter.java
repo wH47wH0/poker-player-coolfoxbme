cdaf0343511551b77fd536aeb46f46f888a12617
György Hamar <gyurinak@gmail.com>
2017-01-14 10:40:52 +0100

    valueconverter

---
+++ src/main/java/org/leanpoker/player/ValueConverter.java
@@ -0,0 +1,22 @@
+package org.leanpoker.player;
+
+/**
+ * Created by hamargyuri on 2017. 01. 14..
+ */
+public abstract class ValueConverter {
+
+    public int convertFaceValue(String rawValue) {
+        switch (rawValue) {
+            case "J":
+                return 11;
+            case "Q":
+                return 12;
+            case "K":
+                return 13;
+            case "A":
+                return 14;
+            default:
+                return Integer.parseInt(rawValue);
+        }
+    }
+}
