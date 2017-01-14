a649d923c0c34c9bd31727d1936575d6d9902133
György Hamar <gyurinak@gmail.com>
2017-01-14 11:43:59 +0100

    Hand initial

--- src/main/java/org/leanpoker/player/Hand.java
+++ src/main/java/org/leanpoker/player/Hand.java
@@ -1,19 +1,51 @@
 package org.leanpoker.player;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by hamargyuri on 2017. 01. 14..
  */
 public class Hand {
-    private final String ALLCARD = "234567891011121314";
-    public static boolean poker(List<>) {
+    private final String STRAIGHT = "234567891011121314";
+
+    public static boolean hasPoker(List<Card> hand) {
+        return sameRanksCheck(hand).contains(4);
+    }
+
+    public static boolean hasDrill(List<Card> hand){
+        return sameRanksCheck(hand).contains(3);
+    }
+
+    public static boolean hasPair(List<Card> hand) {
+        return sameRanksCheck(hand).contains(2);
+    }
+
+    public static boolean hasFull(List<Card> hand) {
+        return hasPair(hand) && hasDrill(hand);
+    }
+
+    public static boolean hasFlush(List<Card> hand){
 
     }
 
-    public static boolean pair;
-    public static boolean flush;
-    public static boolean straight;
-    public static boolean drill;
+    public static boolean hasStraight(List<Card> hand){
 
+    }
+
+
+    private static List<Integer> sameRanksCheck(List<Card> hand) {
+        List<Integer> sameRanks = new ArrayList<>();
+        List<Card> compareList = hand;
+        for (Card card : hand) {
+            int timesInHand = 0;
+            for (Card compareCard : compareList) {
+                if (card.getRank() == compareCard.getRank()) {
+                    timesInHand += 1;
+                }
+            }
+            sameRanks.add(timesInHand);
+        }
+        return sameRanks;
+    }
 }
