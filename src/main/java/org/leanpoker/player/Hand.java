cc8257dcc8a5f110ace0f5f0a46ff53790cadd95
György Hamar <gyurinak@gmail.com>
2017-01-14 11:17:04 +0100

    hand

---
+++ src/main/java/org/leanpoker/player/Hand.java
@@ -0,0 +1,19 @@
+package org.leanpoker.player;
+
+import java.util.List;
+
+/**
+ * Created by hamargyuri on 2017. 01. 14..
+ */
+public class Hand {
+    private final String ALLCARD = "234567891011121314";
+    public static boolean poker(List<>) {
+
+    }
+
+    public static boolean pair;
+    public static boolean flush;
+    public static boolean straight;
+    public static boolean drill;
+
+}
