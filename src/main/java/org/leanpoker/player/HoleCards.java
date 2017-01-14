0e1d22344f3309f8fa4a968cad0646fb76890e8d
radaladar <bbiczok@gmail.com>
2017-01-14 11:16:06 +0100

    .

--- src/main/java/org/leanpoker/player/HoleCards.java
+++ src/main/java/org/leanpoker/player/HoleCards.java
@@ -1,5 +1,7 @@
 package org.leanpoker.player;
 
+import com.sun.beans.editors.IntegerEditor;
+
 /**
  * Created by miki on 2017. 01. 14..
  */
@@ -12,5 +14,10 @@ public class HoleCards {
 
     private boolean aceXhads(Integer val1, Integer val2) {
         return val1 == 14 || val2 == 14;
+    }
+
+    private boolean highcards(Integer val1, Integer val2, Integer high) {
+        high = 8;
+        return val1 >= high && val2 >= high;
     }
 }
